a7c600124c04218cfa6e2145ccbc18e7925529de
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-18 17:09:56 -0300

    Injeção de dependências

---
+++ src/main/java/io/github/thiago/vendas/Controller/ClientesRepository.java
@@ -0,0 +1,13 @@
+package io.github.thiago.vendas.Controller;
+
+
+import io.github.thiago.vendas.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente){
+
+    }
+    }
+
