6197c550c7637ef5b019a7d10896b3b54ce4e36a
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-26 17:00:56 -0300

    Internacionalização produtos

--- src/main/java/io/github/thiago/vendas/domain/entity/Cliente.java
+++ src/main/java/io/github/thiago/vendas/domain/entity/Cliente.java
@@ -21,13 +21,13 @@ public class Cliente {
     private Integer id;
 
     @Column(name ="nome", length = 100)
-    @NotEmpty(message = "Campo nome é obrigatório.")
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
 
     @Column(name = "cpf", length = 11)
-    @NotEmpty(message = "Campo CPF é obrigatório.")
-    @CPF(message = "informe um CPF válido.")
+    @NotEmpty(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
     @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch =  FetchType.LAZY)
