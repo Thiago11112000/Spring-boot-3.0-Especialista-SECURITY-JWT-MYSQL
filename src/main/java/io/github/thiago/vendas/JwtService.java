a6d2905c60c1384a19d0996e5f98c6c7fcbd709b
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-04-01 21:28:03 -0300

    decodificando o token

--- src/main/java/io/github/thiago/vendas/JwtService.java
+++ src/main/java/io/github/thiago/vendas/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.thiago.vendas;
 
 import io.github.thiago.vendas.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
@@ -35,6 +38,30 @@ public class JwtService {
                 .compact();
     }
 
+    public Claims obterClaims(String token)  throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido( String token ){
+        try{
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant()
+                            .atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        }catch (Exception e){
+            return false;
+        }
+    }
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
+    }
+
     public static void main(String[] args) {
         ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
         JwtService  service =  contexto.getBean(JwtService.class);
@@ -42,5 +69,12 @@ public class JwtService {
         String token = service.gerarToken(usuario);
         System.out.println(token);
 
+
+        boolean isTokenValido = service.tokenValido(token);
+        System.out.println("O token está valido ?" + isTokenValido);
+
+        System.out.println(service.obterLoginUsuario(token));
+
     }
 }
+
