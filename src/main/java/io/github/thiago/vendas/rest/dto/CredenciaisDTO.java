bc5c03589cbdae4463890975c187eff0469e3124
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-04-02 14:37:31 -0300

    Implementando o método de autenticacao de usuarios

---
+++ src/main/java/io/github/thiago/vendas/rest/dto/CredenciaisDTO.java
@@ -0,0 +1,11 @@
+package io.github.thiago.vendas.rest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredenciaisDTO {
+    private  String login;
+    private String senha;
+}
---
+++ src/main/java/io/github/thiago/vendas/rest/dto/TokenDTO.java
@@ -0,0 +1,14 @@
+package io.github.thiago.vendas.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+    private String login;
+    private String token;
+}
+
