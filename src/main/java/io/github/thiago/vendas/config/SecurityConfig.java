82128674f04c7502b75b78230998cc353c7cd290
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-04-02 11:30:46 -0300

    Finalizando a configuração do JWT no spring security

--- src/main/java/io/github/thiago/vendas/config/SecurityConfig.java
+++ src/main/java/io/github/thiago/vendas/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.thiago.vendas.config;
 
+import io.github.thiago.vendas.security.jwt.JwtAuthFilter;
+import io.github.thiago.vendas.security.jwt.JwtService;
 import io.github.thiago.vendas.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,18 +10,27 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 @Autowired
 private UsuarioServiceImpl usuarioService;
+@Autowired
+private JwtService jwtService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
 
+    }
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+         return new JwtAuthFilter(jwtService,usuarioService);
     }
 
     @Override
@@ -28,6 +39,7 @@ private UsuarioServiceImpl usuarioService;
                userDetailsService(usuarioService)
                .passwordEncoder(passwordEncoder());
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -43,7 +55,11 @@ private UsuarioServiceImpl usuarioService;
                     .antMatchers(HttpMethod.POST, "/api/usuarios/**")
                 .permitAll()
                     .and()
-                    .httpBasic()
+                    .sessionManagement()
+                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+
 
             ;
     }
